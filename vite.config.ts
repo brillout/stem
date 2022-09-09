385971214935aec72662a5b44a8db68ea53881ec
Romuald Brillout <git@brillout.com>
2022-09-09 17:16:10 +0200

    break down

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-// vite.config.js
 import type { UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 const config: UserConfig = {
   plugins: [ react() ],
@@ -8,5 +8,14 @@ const config: UserConfig = {
     host: true,
     port: 3000,
   },
+  build: {
+    rollupOptions: {
+      input: {
+        p1: path.resolve(__dirname, 'index.html'),
+        p2: path.resolve(__dirname, 'builder.html'),
+        p3: path.resolve(__dirname, 'vision.html')
+      }
+    }
+  }
 }
 export default config
